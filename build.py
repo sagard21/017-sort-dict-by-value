6acfb60adcde5d25bf2d3934aba0486f16a790ad
sagard21 <sagard21@users.noreply.github.com>
2017-08-28 19:31:53 +0000

    Done

--- build.py
+++ build.py
@@ -2,13 +2,10 @@ import operator
 
 
 def solution_asc(dic):
-    '''
-    Enter your code here
-    '''
+    lst1 = sorted(list(dic.items()), key = operator.itemgetter(0))
+    return lst1
 
 
 def solution_desc(dic):
-    '''
-    Enter your code here
-    '''
-
+    lst2 = sorted(list(dic.items()), key = operator.itemgetter(0), reverse = True)
+    return lst2
